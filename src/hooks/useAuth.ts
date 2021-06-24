a7909ec38ddd65ea0b05fc75996cdea0d2fb64d0
sagawa <sagawa.fumi@gmail.com>
2021-06-24 21:16:07 +0900

    add loginmessage

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,12 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
+
   const [loading, setLoading] = useState(false);
 
   const login = useCallback(
@@ -14,19 +17,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
         .catch(() => {
-          alert("ログインできません");
+          showMessage({ title: "ログインできません", status: "error" });
         })
         .finally(() => {
           setLoading(false);
         });
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
